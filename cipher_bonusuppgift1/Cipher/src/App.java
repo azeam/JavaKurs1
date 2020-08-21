90590ecd7fd513e9512eaa4ab96672f03da96451
azeam <dennishagg@gmail.com>
2020-08-21 16:37:11 +0200

    enklare version...

--- cipher_bonusuppgift1/Cipher/src/App.java
+++ cipher_bonusuppgift1/Cipher/src/App.java
@@ -1,27 +1,24 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        Map<Integer,Character> map = new HashMap<Integer,Character>();
-        
-        for(int i=0; i < 26; i++) {
-            map.put(i,(char) (i + 97)); // map char to decimal value and add to hashmap with index 0-26 (only for lowercase letters = decimal values 97-122)
-        }
-
-        String str = "welcome to java programming"; // cipher is -10
-
+        String str = "welcome to java programming"; // cipher is +10/-16
+        char[] buffer = new char[str.length()];
         for(int i=0; i < str.length();i++){
+
             int input = (int) str.charAt(i); // map char in string to decimal value
-            if (input == 32) {
-                System.out.println(" "); // print space instead of null
-            }
-            else if (input >= 113) { // 122 -10 + 1
-                System.out.println(map.get((input - 113))); 
-            }
-            else { // bigger than 10, start from end of alphabet
-                System.out.println(map.get((input - 87)));
+            if (input == 32 || (input >= 97 && input <=122)) { // only print lowercase
+                if (input == 32) {
+                    buffer[i] = ' ';
+                    System.out.println(" "); // print space instead of null
+                }
+                else if (input >= 113) { // 97 + 16, too big to fit, start alphabet from the beginning
+                    buffer[i] = (char) (input - 16);
+                }
+                else { // fits in alphabet, add 10
+                    buffer[i] = (char) (input + 10);
+                }
             }
         }
+        String encoded = new String(buffer);
+        System.out.println(encoded);
     }
 }
